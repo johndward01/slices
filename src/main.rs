3f3b4b7cdddf808d3064fa37fe80ca5c03195a55
johndward01 <johndward01@gmail.com>
2023-05-12 17:35:02 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    //println!("{}", first_word(&String::from("Hello, world!")));
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("the first word is: {}", word);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
